4e08546e3f2ac70586f7b8a41f0b5836a419ab60
David Sterry <sterry.david@gmail.com>
2020-12-07 15:30:35 -0800

    add simple riddle and loop for answer
    Co-authored-by: Jeff Chiu <jwchiu82@gmail.com>

--- Riddles/Program.cs
+++ Riddles/Program.cs
@@ -4,6 +4,18 @@ class Riddles
 {
   static void Main()
   {
-    
+    Console.WriteLine("I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?");
+    string answer = Console.ReadLine();
+
+    bool correct = answer == "fire";
+
+    if (correct)
+    {
+      Console.WriteLine("You are correct, you get to live mortal");
+    }
+    else
+    {
+      Console.WriteLine("You got eaten by the Sphinx, sorry");
+    }
   }
 }
